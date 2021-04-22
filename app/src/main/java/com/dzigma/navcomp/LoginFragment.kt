acc86a753747742af45e15cc4610e7b36c8c373e
Dzikry Maulana Hakim <dzikry.maulana.hakim@gmail.com>
2021-04-22 12:45:22 +0700

    first commit

---
+++ app/src/main/java/com/dzigma/navcomp/LoginFragment.kt
@@ -0,0 +1,44 @@
+package com.dzigma.navcomp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.dzigma.navcomp.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+    private var _binding: FragmentLoginBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonConfirm.setOnClickListener {
+            val username = binding.etUsername.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            val action =
+                LoginFragmentDirections.actionLoginFragmentToWelcomeFragment(username, password)
+            findNavController().navigate(action)
+        }
+    }
+}
